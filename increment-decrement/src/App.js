23ae91232c817c3810cf40cd2f484d94c6fbdc74
eluzung <luzungelaine@gmail.com>
2023-06-15 15:12:53 -0400

    Add react increment-decrement app and added increment and decrement feature

---
+++ increment-decrement/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Component from "./incrementDecrement.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Component className="IncrementDecrement" />
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
